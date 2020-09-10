55f580a7ae6e87d60ca69f80aab31fbd58a13390
shot <shot056@gmail.com>
2020-09-10 15:56:32 +0900

    first cut

---
+++ ColorChime/MainForm.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace ColorChime
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtWavFilePath = new System.Windows.Forms.TextBox();
+            this.lblWavFile = new System.Windows.Forms.Label();
+            this.btnSelectWavFile = new System.Windows.Forms.Button();
+            this.lblWatchInterval = new System.Windows.Forms.Label();
+            this.lblWatchIntervalSec = new System.Windows.Forms.Label();
+            this.numUDWatchInterval = new System.Windows.Forms.NumericUpDown();
+            this.watchTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblTargetArea = new System.Windows.Forms.Label();
+            this.lblTargetColor = new System.Windows.Forms.Label();
+            this.txtTargetColor = new System.Windows.Forms.TextBox();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.btnSelectColor = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numUD_ay = new System.Windows.Forms.NumericUpDown();
+            this.numUD_ax = new System.Windows.Forms.NumericUpDown();
+            this.numUD_by = new System.Windows.Forms.NumericUpDown();
+            this.numUD_bx = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numUDWatchInterval)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_ay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_ax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_by)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_bx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 22);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(93, 11);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // txtWavFilePath
+            // 
+            this.txtWavFilePath.Location = new System.Drawing.Point(88, 79);
+            this.txtWavFilePath.Name = "txtWavFilePath";
+            this.txtWavFilePath.Size = new System.Drawing.Size(271, 19);
+            this.txtWavFilePath.TabIndex = 4;
+            this.txtWavFilePath.TextChanged += new System.EventHandler(this.txtWavFilePath_TextChanged);
+            // 
+            // lblWavFile
+            // 
+            this.lblWavFile.AutoSize = true;
+            this.lblWavFile.Location = new System.Drawing.Point(10, 82);
+            this.lblWavFile.Name = "lblWavFile";
+            this.lblWavFile.Size = new System.Drawing.Size(59, 12);
+            this.lblWavFile.TabIndex = 5;
+            this.lblWavFile.Text = "wavファイル";
+            // 
+            // btnSelectWavFile
+            // 
+            this.btnSelectWavFile.Location = new System.Drawing.Point(365, 77);
+            this.btnSelectWavFile.Name = "btnSelectWavFile";
+            this.btnSelectWavFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectWavFile.TabIndex = 5;
+            this.btnSelectWavFile.Text = "参照";
+            this.btnSelectWavFile.UseVisualStyleBackColor = true;
+            this.btnSelectWavFile.Click += new System.EventHandler(this.btnSelectWavFile_Click);
+            // 
+            // lblWatchInterval
+            // 
+            this.lblWatchInterval.AutoSize = true;
+            this.lblWatchInterval.Location = new System.Drawing.Point(10, 51);
+            this.lblWatchInterval.Name = "lblWatchInterval";
+            this.lblWatchInterval.Size = new System.Drawing.Size(53, 12);
+            this.lblWatchInterval.TabIndex = 9;
+            this.lblWatchInterval.Text = "監視間隔";
+            // 
+            // lblWatchIntervalSec
+            // 
+            this.lblWatchIntervalSec.AutoSize = true;
+            this.lblWatchIntervalSec.Location = new System.Drawing.Point(153, 51);
+            this.lblWatchIntervalSec.Name = "lblWatchIntervalSec";
+            this.lblWatchIntervalSec.Size = new System.Drawing.Size(17, 12);
+            this.lblWatchIntervalSec.TabIndex = 11;
+            this.lblWatchIntervalSec.Text = "秒";
+            // 
+            // numUDWatchInterval
+            // 
+            this.numUDWatchInterval.Location = new System.Drawing.Point(88, 48);
+            this.numUDWatchInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numUDWatchInterval.Name = "numUDWatchInterval";
+            this.numUDWatchInterval.Size = new System.Drawing.Size(59, 19);
+            this.numUDWatchInterval.TabIndex = 3;
+            this.numUDWatchInterval.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // watchTimer
+            // 
+            this.watchTimer.Tick += new System.EventHandler(this.watchTimer_Tick);
+            // 
+            // lblTargetArea
+            // 
+            this.lblTargetArea.AutoSize = true;
+            this.lblTargetArea.Location = new System.Drawing.Point(13, 114);
+            this.lblTargetArea.Name = "lblTargetArea";
+            this.lblTargetArea.Size = new System.Drawing.Size(54, 12);
+            this.lblTargetArea.TabIndex = 13;
+            this.lblTargetArea.Text = "監視エリア";
+            // 
+            // lblTargetColor
+            // 
+            this.lblTargetColor.AutoSize = true;
+            this.lblTargetColor.Location = new System.Drawing.Point(16, 228);
+            this.lblTargetColor.Name = "lblTargetColor";
+            this.lblTargetColor.Size = new System.Drawing.Size(53, 12);
+            this.lblTargetColor.TabIndex = 18;
+            this.lblTargetColor.Text = "監視RGB";
+            // 
+            // txtTargetColor
+            // 
+            this.txtTargetColor.Location = new System.Drawing.Point(88, 225);
+            this.txtTargetColor.Name = "txtTargetColor";
+            this.txtTargetColor.Size = new System.Drawing.Size(100, 19);
+            this.txtTargetColor.TabIndex = 10;
+            this.txtTargetColor.Text = "#000000";
+            this.txtTargetColor.TextChanged += new System.EventHandler(this.txtTargetColor_TextChanged);
+            // 
+            // btnSelectColor
+            // 
+            this.btnSelectColor.Location = new System.Drawing.Point(194, 223);
+            this.btnSelectColor.Name = "btnSelectColor";
+            this.btnSelectColor.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectColor.TabIndex = 11;
+            this.btnSelectColor.Text = "選択";
+            this.btnSelectColor.UseVisualStyleBackColor = true;
+            this.btnSelectColor.Click += new System.EventHandler(this.btnSelectColor_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.numUD_ay);
+            this.panel1.Controls.Add(this.numUD_ax);
+            this.panel1.Controls.Add(this.numUD_by);
+            this.panel1.Controls.Add(this.numUD_bx);
+            this.panel1.Location = new System.Drawing.Point(88, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(7, 12);
+            this.label1.TabIndex = 35;
+            this.label1.Text = ",";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(7, 12);
+            this.label4.TabIndex = 35;
+            this.label4.Text = ",";
+            // 
+            // numUD_ay
+            // 
+            this.numUD_ay.Location = new System.Drawing.Point(57, -1);
+            this.numUD_ay.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numUD_ay.Name = "numUD_ay";
+            this.numUD_ay.Size = new System.Drawing.Size(50, 19);
+            this.numUD_ay.TabIndex = 7;
+            // 
+            // numUD_ax
+            // 
+            this.numUD_ax.Location = new System.Drawing.Point(0, -1);
+            this.numUD_ax.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numUD_ax.Name = "numUD_ax";
+            this.numUD_ax.Size = new System.Drawing.Size(50, 19);
+            this.numUD_ax.TabIndex = 6;
+            // 
+            // numUD_by
+            // 
+            this.numUD_by.Location = new System.Drawing.Point(149, 80);
+            this.numUD_by.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numUD_by.Name = "numUD_by";
+            this.numUD_by.Size = new System.Drawing.Size(50, 19);
+            this.numUD_by.TabIndex = 9;
+            // 
+            // numUD_bx
+            // 
+            this.numUD_bx.Location = new System.Drawing.Point(92, 80);
+            this.numUD_bx.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numUD_bx.Name = "numUD_bx";
+            this.numUD_bx.Size = new System.Drawing.Size(50, 19);
+            this.numUD_bx.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 260);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSelectColor);
+            this.Controls.Add(this.txtTargetColor);
+            this.Controls.Add(this.lblTargetColor);
+            this.Controls.Add(this.lblTargetArea);
+            this.Controls.Add(this.numUDWatchInterval);
+            this.Controls.Add(this.lblWatchIntervalSec);
+            this.Controls.Add(this.lblWatchInterval);
+            this.Controls.Add(this.btnSelectWavFile);
+            this.Controls.Add(this.lblWavFile);
+            this.Controls.Add(this.txtWavFilePath);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "ColorChime";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numUDWatchInterval)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_ay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_ax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_by)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUD_bx)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox txtWavFilePath;
+        private System.Windows.Forms.Label lblWavFile;
+        private System.Windows.Forms.Button btnSelectWavFile;
+        private System.Windows.Forms.Label lblWatchInterval;
+        private System.Windows.Forms.Label lblWatchIntervalSec;
+        private System.Windows.Forms.NumericUpDown numUDWatchInterval;
+        private System.Windows.Forms.Timer watchTimer;
+        private System.Windows.Forms.Label lblTargetArea;
+        private System.Windows.Forms.Label lblTargetColor;
+        private System.Windows.Forms.TextBox txtTargetColor;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Button btnSelectColor;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numUD_ay;
+        private System.Windows.Forms.NumericUpDown numUD_ax;
+        private System.Windows.Forms.NumericUpDown numUD_by;
+        private System.Windows.Forms.NumericUpDown numUD_bx;
+    }
+}
+
---
+++ ColorChime/MainForm.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Xml.Schema;
+
+namespace ColorChime
+{
+    public partial class MainForm : Form
+    {
+        private static Timer timer = new Timer();
+        private Color targetColor = Color.FromArgb(0,0,0);
+        private System.Media.SoundPlayer player = null;
+        private string configPath;
+        public MainForm()
+        {
+            InitializeComponent();
+            timer.Tick += new EventHandler(watchTimer_Tick);
+            string appPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string parentPath = System.IO.Directory.GetParent(appPath).FullName;
+            configPath = parentPath + "\\config.xml";
+        }
+
+
+
+        private static Image CaptureActiveWindowWithPrtScr()
+        {
+            SendKeys.SendWait("%{PRTSC}");
+            IDataObject d = Clipboard.GetDataObject();
+            if( d != null )
+            {
+                Image img = (Image)d.GetData(DataFormats.Bitmap);
+                if( img != null )
+                {
+                    return img;
+                }
+            }
+            return null;
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(configPath))
+            {
+                System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(ColorChimeConfig));
+                System.IO.StreamReader sr = new System.IO.StreamReader(configPath, new System.Text.UTF8Encoding(false));
+                ColorChimeConfig config = (ColorChimeConfig)serializer.Deserialize(sr);
+                sr.Close();
+
+                txtWavFilePath.Text      = config.waveFilePath;
+                numUDWatchInterval.Value = config.watchInterval;
+                numUD_ax.Value           = config.pointAx;
+                numUD_ay.Value           = config.pointAy;
+                numUD_bx.Value           = config.pointBx;
+                numUD_by.Value           = config.pointBy;
+                txtTargetColor.Text      = config.watchColor;
+            }
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ColorChimeConfig config = new ColorChimeConfig();
+            config.waveFilePath = txtWavFilePath.Text;
+            config.watchInterval = (int)numUDWatchInterval.Value;
+            config.pointAx = (int)numUD_ax.Value;
+            config.pointAy = (int)numUD_ay.Value;
+            config.pointBx = (int)numUD_bx.Value;
+            config.pointBy = (int)numUD_by.Value;
+            config.watchColor = txtTargetColor.Text;
+
+
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(ColorChimeConfig));
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(configPath, false, new System.Text.UTF8Encoding(false));
+            serializer.Serialize(sw, config);
+            sw.Close();
+        }
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            timer.Interval = (int)numUDWatchInterval.Value * 1000;
+            timer.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void btnSelectWavFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Wavファイル(*.wav)|*.wav";
+            if( dlg.ShowDialog(this) == DialogResult.OK )
+            {
+                txtWavFilePath.Text = dlg.FileName;
+            }
+        }
+
+        private void watchTimer_Tick(object sender, EventArgs e)
+        {
+            using (var img = CaptureActiveWindowWithPrtScr())
+            {
+                img.Save(System.IO.Path.GetTempPath() + "\\ColorChime_full.png", ImageFormat.Png);
+                int ax = (int)numUD_ax.Value;
+                int ay = (int)numUD_ay.Value;
+                int bx = (int)numUD_bx.Value;
+                int by = (int)numUD_by.Value;
+                Rectangle srcRect = new Rectangle(ax, ay, bx-ax, by-ay );
+                Bitmap dest = new Bitmap(srcRect.Width, srcRect.Height);
+                using (Graphics g = Graphics.FromImage(dest))
+                {
+                    g.DrawImage(img, new Rectangle(0, 0, srcRect.Width, srcRect.Height), srcRect, GraphicsUnit.Pixel);
+                    dest.Save(System.IO.Path.GetTempPath() + "\\ColorChime_trimed.png", ImageFormat.Png);
+                    g.Dispose();
+                    if( findColorInImage( dest, targetColor ) )
+                    {
+                        player.PlaySync();
+                        Debug.WriteLine("find color");
+                    }
+                }
+                Debug.WriteLine("save img");
+            }
+        }
+
+        private Boolean findColorInImage( Bitmap bmp, Color color)
+        {
+            Debug.WriteLine(color);
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    Debug.WriteLine("{0} : {1}", color, c);
+                    if ( c == color )
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private void btnSelectColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if( dlg.ShowDialog(this) == DialogResult.OK )
+            {
+                targetColor = dlg.Color;
+                txtTargetColor.Text = String.Format("#{0:X2}{1:X2}{2:X2}", dlg.Color.R, dlg.Color.G, dlg.Color.B);
+            }
+        }
+
+        private void txtTargetColor_TextChanged(object sender, EventArgs e)
+        {
+            if (txtTargetColor.Text.Length == 7)
+            {
+                Debug.WriteLine(String.Format("{0} = {1} : {2} : {3}", txtTargetColor.Text,
+                                                                       txtTargetColor.Text.Substring(1, 2),
+                                                                       txtTargetColor.Text.Substring(3, 2),
+                                                                       txtTargetColor.Text.Substring(5, 2)));
+                int red   = Convert.ToInt32(txtTargetColor.Text.Substring(1, 2),16);
+                int green = Convert.ToInt32(txtTargetColor.Text.Substring(3, 2),16);
+                int blue  = Convert.ToInt32(txtTargetColor.Text.Substring(5, 2),16);
+                Color color = Color.FromArgb(red, green, blue);
+                targetColor = color;
+            }
+
+        }
+
+        private void txtWavFilePath_TextChanged(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(txtWavFilePath.Text))
+            {
+                if (player != null)
+                    StopSound();
+                player = new System.Media.SoundPlayer(txtWavFilePath.Text);
+            }
+        }
+        private void StopSound()
+        {
+            if (player != null)
+            {
+                player.Stop();
+                player.Dispose();
+                player = null;
+            }
+        }
+    }
+
+    public class ColorChimeConfig
+    {
+        private static int _watchInterval;
+        private static String _waveFilePath;
+        private static int _pointAx;
+        private static int _pointAy;
+        private static int _pointBx;
+        private static int _pointBy;
+        private static String _watchColor;
+
+        public int watchInterval
+        {
+            get { return _watchInterval;  }
+            set { _watchInterval = value; }
+        }
+        public String waveFilePath
+        {
+            get { return _waveFilePath; }
+            set { _waveFilePath = value; }
+        }
+        public int pointAx
+        {
+            get { return _pointAx; }
+            set { _pointAx = value; }
+        }
+        public int pointAy
+        {
+            get { return _pointAy; }
+            set { _pointAy = value; }
+        }
+        public int pointBx
+        {
+            get { return _pointBx; }
+            set { _pointBx = value; }
+        }
+        public int pointBy
+        {
+            get { return _pointBy; }
+            set { _pointBy = value; }
+        }
+        public String watchColor
+        {
+            get { return _watchColor; }
+            set { _watchColor = value; }
+        }
+    }
+}
